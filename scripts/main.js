52fb06b9a84c139e3c1573f592b442231a03110e
chrisvillan <villanuevachris19@gmail.com>
2018-03-08 13:10:11 -0800

    added checklist module

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
 (function (window) {
   "use strict";
   var FORM_SELECTOR = "[data-coffee-order=\"form\"]";
+  var FORM_PAYMENT = "[data-payment-info=\"form\"]";
+  var CHECKLIST_SELECTOR = "[data-coffee-order=\"checklist\"]";
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
   var myTruck = new Truck("ncc-1701", new DataStore());
   window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
-
-  var FORM_PAYMENT = "[data-payment-info=\"form\"]";
   var paymentHandler = new FormHandler(FORM_PAYMENT);
 
 
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addSubmitHandler(function (data){
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
   paymentHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  console.log(formHandler);
+
 })(window);
